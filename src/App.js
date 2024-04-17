a5d484933f77abf7f3ca28fc9031afc159573126
Samilly <samillynunes19@gmail.com>
2024-04-17 18:25:27 -0300

    use state and use effects applied

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+function App(){
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  function handleAdd(){
+
+    setTasks([...tasks, input]);
+    setInput('');
+  }
+
+  // Quando a gente deixa o paramatro de observacao (segundO) vazio, ele vai executar isso
+  // assim que montar a tela
+  useEffect(()=>{
+    const tasksStorage = localStorage.getItem('tasks');
+
+    // Se tem alguma coisa na lista...
+    if(tasksStorage){
+      setTasks(JSON.parse(tasksStorage));
+    }
+
+    // A linha abaixo trabalha equivalente ao componente willMount():
+    return () => {}
+  }, []);
+  
+  useEffect(()=>{
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> React Hooks</h1>
+      <ul>
+        { tasks.map(task => 
+            <li key={task}> { task } </li> 
+        )}
+      </ul>
+      <input type="text" value={input} onChange={(e)=> setInput(e.target.value)} />
+      <button type="button" onClick={handleAdd} >Adicionar</button>
     </div>
   );
 }
